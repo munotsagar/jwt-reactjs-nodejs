a853be16bc19e8a8ecb6cbd239d56d257ec129bb
Sagar Motilal Munot <munot.sagar@gmail.com>
2026-05-14 18:57:50 +0530

    Made changes related to Refresh token

--- backend/src/services/login.js
+++ backend/src/services/login.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user")
 const { generateToken } = require("../utils/jwtUtils");
+const { verifyToken } = require("../utils/authMiddleware");
 
 
 async function login(email, password) {
@@ -21,4 +22,25 @@ async function login(email, password) {
     }
 }
 
-module.exports = login;
+async function refreshToken(token) {
+    try {
+        const decoded = verifyToken(token)
+        if (!decoded) {
+            throw new Error("Invalid token")
+        }
+        const user = await User.findById(decoded.id)
+        if (!user) {
+            throw new Error("User not found")
+        }
+        const newToken = generateToken(user)
+        return newToken;
+    }
+    catch (error) {
+        console.log("Error : ", error.message)
+        throw new Error("Unable to refresh token")
+    }
+}
+
+
+
+module.exports = {login, refreshToken};
